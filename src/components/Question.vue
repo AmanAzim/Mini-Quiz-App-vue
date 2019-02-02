3ec7cfd24d55ad933ce89ec999f103e4c383d36d
Aman <rumman.djuice1@gmail.com>
2019-02-02 22:11:25 +0100

    commit

---
+++ src/components/Question.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title text-center">{{ question }}</h3>
+        </div>
+        <div class="panel-body">
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[0].correct)">{{ btnData[0].answer }}</button>
+            </div>
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[1].correct)">{{ btnData[1].answer }}</button>
+            </div>
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[2].correct)">{{ btnData[2].answer }}</button>
+            </div>
+            <div class="col-xs-12 col-sm-6 text-center">
+                <button class="btn btn-primary btn-lg" style="margin: 10px" @click="onAnswer(btnData[3].correct)">{{ btnData[3].answer }}</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    const MODE_ADDITION = 1;
+    const MODE_SUBTRACTION = 2;
+    export default {
+        data(){
+            return{
+                question: 'Oops, an error ocurred :/',
+                btnData:[
+                    {correct:true, answer:0},
+                    {correct:false, answer:0},
+                    {correct:false, answer:0},
+                    {correct:false, answer:0}
+                    ],
+            };
+        },
+        methods:{
+            generateQuestion(){
+
+            },
+            generateRandomNumber(){
+
+            },
+            onAnswer(isCorrect){
+                this.$emit('answered', isCorrect);
+            }
+        },
+        created(){
+            this.generateQuestion();
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
