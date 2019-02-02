dfad7ae1417d89fab6fa87b3bbdae452db953480
Aman <rumman.djuice1@gmail.com>
2019-02-02 23:53:50 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -47,5 +47,33 @@
 </script>
 
 <style>
+.flip-enter{
+
+}
+.flip-enter-active{
+    animation:flip-in 0.5s ease-out forwards;
+}
+.flip-leave{
+
+}
+.flip-leave-active{
+    animation:flip-out 0.5s ease-out forwards;
+}
+@keyframes flip-out {
+    from{
+        transform: rotateY(0deg);
+    }
+    to{
+        transform: rotateY(90deg);
+    }
+}
+@keyframes flip-in {
+    from{
+        transform: rotateY(90deg);
+    }
+    to{
+        transform: rotateY(0deg);
+    }
+}
 
 </style>
